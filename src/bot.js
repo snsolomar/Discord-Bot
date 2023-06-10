9166be06e9dba958c65bf35e799bdfd64f6fca26
Simon Solomon <snsolomar@gmail.com>
2023-06-10 13:29:26 -0500

    client with intents

--- src/bot.js
+++ src/bot.js
@@ -1,3 +1,14 @@
 require('dotenv').config();
 
+// Import the bot
+const { Client, IntentsBitField} = require('discord.js');
+
+// Intents are a set of permissions you bots can use to get access to a set of events
+const client = new Client({
+    intents: [
+        IntentsBitField.Flags.Guilds,
+
+    ]
+})
+
 
