26302badeaec963b5c7255f07f7efef5d1740da6
Simon Solomon <snsolomar@gmail.com>
2023-06-10 20:39:34 -0500

    login

--- src/bot.js
+++ src/bot.js
@@ -1,14 +1,23 @@
 require('dotenv').config();
 
+const loginToken = process.env.BOT_TOKEN;
+
+// console.log(loginToken);
+
 // Import the bot
 const { Client, IntentsBitField} = require('discord.js');
 
 // Intents are a set of permissions you bots can use to get access to a set of events
+// List of Itents can be found in the following url: https://discord.com/developers/docs/topics/gateway#gateway-intents
 const client = new Client({
     intents: [
         IntentsBitField.Flags.Guilds,
+        IntentsBitField.Flags.GuildMembers,
+        IntentsBitField.Flags.GuildMessages,
+        IntentsBitField.Flags.MessageContent,
+    ],
+});
 
-    ]
-})
+client.login(loginToken)
 
 
