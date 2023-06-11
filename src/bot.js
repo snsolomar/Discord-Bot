e46cea0fa8e4619f936f3abae1e25d3cb1887494
Simon Solomon <snsolomar@gmail.com>
2023-06-10 21:37:56 -0500

    coingeck token price

--- src/bot.js
+++ src/bot.js
@@ -1,11 +1,15 @@
 require('dotenv').config();
 
+const discord = require('discord.js')
+const axios = require('axios');
+const tokenPricePrefix = '$';
+
 const loginToken = process.env.BOT_TOKEN;
 
 // console.log(loginToken);
 
 // Import the bot
-const { Client, IntentsBitField} = require('discord.js');
+const { Client, IntentsBitField} = discord;
 
 // Intents are a set of permissions you bots can use to get access to a set of events
 // List of Itents can be found in the following url: https://discord.com/developers/docs/topics/gateway#gateway-intents
@@ -24,14 +28,36 @@ client.on('ready', (c) => {
 });
 
 // This event listener can only work the the intent is provided in the client
-client.on('messageCreate', (message) => {
+client.on('messageCreate', async(message) => {
     // console.log(message.content)
-    if (message.author.bot) {
+    
+    
+    if (!message.content.startsWith(tokenPricePrefix) || message.author.bot) {
         return;
+    }
+
+    const args = message.content.slice(tokenPricePrefix.length).trim().split(' ');
+    const command = args.shift().toLocaleLowerCase();
+
+    if (command === 'token_price') {
+        const token = args[0];
+
+        try {
+            const response = await axios.get(`https://api.coingecko.com/api/v3/simple/price?ids=${token}&vs_currencies=usd`);
+            const price = response.data[token].usd;
+
+            message.channel.send(`The current price of ${token} is $${price}`);
+        } catch (error) {
+            message.channel.send('Token not found');
+        }
     }
 
     if (message.content === 'hello') {
         message.reply("Hey!");
+    }
+
+    if (!message.content.startsWith(tokenPricePrefix) || message.author.bot) {
+        return;
     }
 });
 
