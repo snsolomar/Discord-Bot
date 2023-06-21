8e5c13c934935008759fbc627972afc83930aece
Simon Solomon <snsolomar@gmail.com>
2023-06-20 20:24:58 -0500

    registered commands

--- src/Events/Ready/01registerComands.js
+++ src/Events/Ready/01registerComands.js
@@ -21,7 +21,7 @@ module.exports = async (client) => {
       if (existingCommand) {
         if (localCommand.deleted) {
           await applicationCommands.delete(existingCommand.id);
-          console.log(`🗑 Deleted command "${name}".`);
+          console.log(`Deleted command "${name}".`);
           continue;
         }
 
@@ -31,12 +31,12 @@ module.exports = async (client) => {
             options,
           });
 
-          console.log(`🔁 Edited command "${name}".`);
+          console.log(`Edited command "${name}".`);
         }
       } else {
         if (localCommand.deleted) {
           console.log(
-            `⏩ Skipping registering command "${name}" as it's set to delete.`
+            `Skipping registering command "${name}" as it's set to delete.`
           );
           continue;
         }
@@ -47,7 +47,7 @@ module.exports = async (client) => {
           options,
         });
 
-        console.log(`👍 Registered command "${name}."`);
+        console.log(`Registered command "${name}."`);
       }
     }
   } catch (error) {
